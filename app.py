a1e05235bcb7e12161300924275e5fea310c6488
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-05-15 23:26:45 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -6,22 +6,23 @@ from transformers import BlipProcessor, BlipForQuestionAnswering
 processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-capfilt-large")
 model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-capfilt-large")
 
-def answer_question(image, question):
-    """Answers a question about an image using the Blip model."""
+def blipify_image(image, question):
+    """BLIPifies an image to answer your art exploration question."""
     inputs = processor(image, question, return_tensors="pt")
     out = model.generate(**inputs)
     answer = processor.decode(out[0], skip_special_tokens=True)
     return answer.title()  # Capitalize the first letter of the answer
 
-st.title("Explore Art with BLIP")  # Change title to reflect art theme
-st.subheader("Ask a question about the artwork below")
+st.title("BLIPify: Unlock the Secrets of Art")
+
+st.subheader("BLIPify your artistic curiosity with a question below")
 
 with st.sidebar:
-    image_file = st.file_uploader("Upload an Artwork", type=["jpg", "jpeg", "png", "webp"])
-    question = st.text_area("Your Art Exploration Question")
+    image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png", "webp"])
+    question = st.text_area("Your BLIPification Question")
 
 if image_file and question:
     image = Image.open(image_file)
     st.image(image, caption='Uploaded Artwork', use_column_width=True)
-    answer = answer_question(image, question)
-    st.write(f"**Answer:** {answer}")  # Emphasize answer with bold text
+    answer = blipify_image(image, question)
+    st.write(f"**BLIPified Answer:** {answer}")  # Emphasize answer with bold text
